2bc0df9d0fccd0647f69899dda7a171c36372717
Vitaliy <ps.substance@gmail.com>
2021-06-02 21:38:13 +0300

    c

---
+++ src/js/modules/checkNumInputs.js
@@ -0,0 +1,13 @@
+
+const chekNumInputs = (selector) => {
+
+    const numInputs = document.querySelectorAll(selector);
+
+    numInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            item.value = item.value.replace(/\D/, '');
+        })
+    })
+};
+
+export default chekNumInputs;
---
+++ src/js/modules/modals.js
@@ -0,0 +1,89 @@
+
+const modals = () => {
+
+    const bindModal = (triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) => {
+
+        const trigger = document.querySelectorAll(triggerSelector),
+         modal = document.querySelector(modalSelector),
+         close = document.querySelector(closeSelector),
+         window = document.querySelectorAll('[data-modal]'),
+         scroll = calcScroll();
+
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if ( e.target ) {
+                    e.preventDefault();
+                }
+                
+                window.forEach(item => {
+                    item.style.display ='none';
+                })
+
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";
+                document.body.style.marginRight = `${scroll}px`;
+                // document.body.classList.add('modal-open');
+            });
+        });
+
+        modal.addEventListener("click", (e) => {
+            if (e.target === modal && closeClickOverlay) {
+                window.forEach(item => {
+                    item.style.display ='none';
+                })
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+                document.body.style.marginRight = '0px';
+            // document.body.classList.remove('modal-open');
+            }
+        }); 
+
+        close.addEventListener('click', (e) => {
+            window.forEach(item => {
+                item.style.display ='none';
+            });
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+            document.body.style.marginRight = '0px';
+            // document.body.classList.remove('modal-open');
+        });
+
+              
+    }
+
+    function showModalByTime(selector, time) {
+        setTimeout(() => {
+            document.querySelector(selector).style.display = "block";
+            document.body.style.overflow = "";
+        }, time);
+    }
+
+    function calcScroll(){
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'srcoll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
+
+
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close'); 
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    // showModalByTime('.popup', 60000);
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    //connect modals start
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false); 
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+    // connect modals end
+};
+
+
+export default modals
